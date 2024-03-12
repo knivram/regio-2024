8291e90423d91092251d05e5bbc1a9d316b0fe51
Marvin <marvin.kramer.lol@gmail.com>
2024-03-12 18:35:55 +0100

    added dropdown

---
+++ src/main/kotlin/ui/component/DropDown.kt
@@ -0,0 +1,49 @@
+package ui.component
+
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.runtime.*
+import java.util.UUID
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun DropDown(
+    items: Map<UUID, String>,
+    value: String,
+    onSelect: (id: UUID) -> Unit,
+    label: @Composable (() -> Unit)?,
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = it }
+    ) {
+        OutlinedTextField(
+            value = value,
+            onValueChange = {},
+            readOnly = true,
+            label = label,
+            trailingIcon = {
+                Icon(Icons.Default.ArrowDropDown, contentDescription = "Dropdown")
+            },
+        )
+
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+        ) {
+            items.forEach { (id, value) ->
+                DropdownMenuItem(onClick = {
+                    onSelect(id)
+                    expanded = false
+                }) {
+                    Text(text = value)
+                }
+            }
+        }
+    }
+
+}
